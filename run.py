f35809100eddeb419e3dad22bf48edec0284d0ea
tomoya_chen <tomoya_chen@163.com>
2019-06-28 13:41:50 +0800

    Created

---
+++ run.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+"""
+程序运行入口
+"""
+
+import wechat_avatar
+import avatar_mosaic
+import os
+
+def run():
+    """ 主程序入口"""
+    wechat_avatar.run()
+    # avatar_mosaic.run("template.jpg", "wechat_mosaic.jpg")
+    avatar_mosaic.run(os.getcwd() + "\\wechat\\" + "0.jpg" "wechat_mosaic.jpg")
+
+
+if __name__ == '__main__':
+    run()
---
+++ wechat_avatar.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+from wxpy import *
+import math
+from PIL import Image
+import os
+
+# 创建头像存放文件夹
+def creat_filepath():
+    avatar_dir = os.getcwd() + "\\wechat\\"
+    if not os.path.exists(avatar_dir):
+        os.mkdir(avatar_dir)
+    return avatar_dir
+
+# 保存好友头像
+def save_avatar(avatar_dir):
+    # 初始化机器人，扫码登陆
+    bot = Bot()
+    friends = bot.friends(update=True)
+    num = 0
+    for friend in friends:
+        friend.get_avatar(avatar_dir + '\\' + str(num) + ".jpg")
+        print('好友昵称:%s' % friend.nick_name)
+        num = num + 1
+
+# 拼接头像
+def joint_avatar(path):
+    # 获取文件夹内头像个数
+    length = len(os.listdir(path))
+    # 设置画布大小
+    image_size = 1080
+    # 设置每个头像大小
+    each_size = math.ceil(image_size / math.floor(math.sqrt(length)))
+    # 计算所需各行列的头像数量
+    x_lines = math.floor(math.sqrt(length))
+    y_lines = math.floor(math.sqrt(length))
+    print('length', length)
+    print('x_lines', x_lines)
+    print('y_lines', y_lines)
+    image = Image.new('RGB', (each_size * x_lines, each_size * y_lines))
+    x = 0
+    y = 0
+    for (root, dirs, files) in os.walk(path):
+        for pic_name in files:
+            # 增加头像读取不出来的异常处理
+                try:
+                    with Image.open(path + pic_name) as img:
+                        img = img.resize((each_size, each_size))
+                        image.paste(img, (x * each_size, y * each_size))
+                        x += 1
+                        if x == x_lines:
+                            x = 0
+                            y += 1
+                except IOError:
+                    print("头像读取失败")
+
+    img = image.save(os.getcwd() + "/wechat.png")
+    print('微信好友头像拼接完成!')
+
+
+def run():
+    avatar_dir = creat_filepath()
+    save_avatar(avatar_dir)
+    joint_avatar(avatar_dir)
+    return
+
+if __name__ == '__main__':
+    avatar_dir = creat_filepath()
+    save_avatar(avatar_dir)
+    joint_avatar(avatar_dir)
